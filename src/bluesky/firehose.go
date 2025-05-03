c2cda0cf914d4f723b59069f66dd46c656a26e44
Josh <joshua.childs01@outlook.com>
2025-05-03 20:33:14 +1000

    bluesky firehose updates

--- src/bluesky/firehose.go
+++ src/bluesky/firehose.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 	"time"
 
 	"github.com/bluesky-social/indigo/api/atproto"
+	appbsky "github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/events"
 	"github.com/bluesky-social/indigo/events/schedulers/sequential"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
+	"github.com/bluesky-social/indigo/repo"
+	"github.com/bluesky-social/indigo/repomgr"
 	"github.com/gorilla/websocket"
 )
+
+type MockHTTPResponse struct {
+}
+
+func (m *MockHTTPResponse) Write(buf []byte) (int, error) {
+	fmt.Printf("%s", buf)
+
+	return 0, nil
+}
+
+func (m *MockHTTPResponse) WriteHeader(status int) {
+
+}
+
+func (m *MockHTTPResponse) Header() http.Header {
+	return make(http.Header)
+}
+
+func main() {
+	Handler(&MockHTTPResponse{}, nil)
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	uri := "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos"
@@ -22,34 +47,59 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	var l sync.Mutex
-	array := make([]string, 0, 50)
-
+	ctx, closed := context.WithCancel(context.Background())
 	rsc := &events.RepoStreamCallbacks{
 		RepoCommit: func(evt *atproto.SyncSubscribeRepos_Commit) error {
-			for _, op := range evt.Ops {
-				s := fmt.Sprintf("%s: %s record from %s", evt.Repo, op.Action, op.Path)
+			rr, err := repo.ReadRepoFromCar(ctx, bytes.NewReader(evt.Blocks))
+			if err != nil {
+				fmt.Println(err)
+			}
 
-				l.Lock()
-				array = append(array, s)
-				l.Unlock()
+			for _, op := range evt.Ops {
+				if repomgr.EventKind(op.Action) != repomgr.EvtKindCreateRecord {
+					continue
+				}
+
+				rc, rec, err := rr.GetRecord(ctx, op.Path)
+				if err != nil {
+					e := fmt.Errorf("getting record %s (%s) within seq %d for %s: %w", op.Path, *op.Cid, evt.Seq, evt.Repo, err)
+					log.Println(e)
+				}
+
+				if lexutil.LexLink(rc) != *op.Cid {
+					return fmt.Errorf("mismatch in record and op cid: %s != %s", rc, *op.Cid)
+				}
+
+				banana := lexutil.LexiconTypeDecoder{
+					Val: rec,
+				}
+
+				b, err := banana.MarshalJSON()
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				var pst = appbsky.FeedPost{}
+				err = json.Unmarshal(b, &pst)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				if pst.LexiconTypeID != "app.bsky.feed.post" {
+					continue
+				}
+
+				fmt.Println(pst)
 			}
 			return nil
 		},
 	}
 
-	ctx, closed := context.WithCancel(context.Background())
 	sched := sequential.NewScheduler("myfirehose", rsc.EventHandler)
 	go events.HandleRepoStream(ctx, con, sched, nil)
 
 	time.Sleep(time.Second)
 	closed()
 
-	l.Lock()
-	buf, err := json.Marshal(array)
-	if err != nil {
-		log.Println(err)
-	}
-
-	w.Write(buf)
+	log.Println("firehose closed")
 }
