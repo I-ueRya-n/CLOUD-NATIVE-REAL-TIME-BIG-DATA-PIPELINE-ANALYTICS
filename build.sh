30bc3ad8eb49095e189abcc6b86f559eed904f83
root <root@IUE>
2025-05-09 15:23:44 +1000

    Resolve conflict and apply stashed changes

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+pip3 install -r ${SRC_PKG}/requirements.txt -t ${SRC_PKG}
+cp -r ${SRC_PKG}/* ${DEPLOY_PKG}/
--- src/reddit/build.sh
+++ src/reddit/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+<<<<<<< HEAD
 
 
 mkdir -p ${DEPLOY_PKG}
@@ -9,3 +10,9 @@ pip install --upgrade -r ${SRC_PKG}/requirements.txt -t /tmp/dep
 
 cp -r /tmp/dep/* ${DEPLOY_PKG}/
 cp ${SRC_PKG}/*.py ${DEPLOY_PKG}/
+=======
+set -e
+
+pip3 install -r ${SRC_PKG}/requirements.txt -t ${SRC_PKG}
+cp -r ${SRC_PKG}/* ${DEPLOY_PKG}/
+>>>>>>> 7aaad7a (Save local changes before rebase)
