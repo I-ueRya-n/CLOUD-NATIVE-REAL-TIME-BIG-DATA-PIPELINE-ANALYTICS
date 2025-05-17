496e729253411a5e8f9c310d46f041d385154576
Ryan LIN <yuwei.lin1@student.unimelb.edu.au>
2025-05-18 02:35:06 +1000

    Edit app_to_fission_redis.py

--- src/app_to_fission_redis.py
+++ src/app_to_fission_redis.py
@@ -7,11 +7,13 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # --- Reddit API 配置 ---
+'''
 REDDIT_CLIENT_ID = os.environ.get("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.environ.get("REDDIT_CLIENT_SECRET")
 REDDIT_USERNAME = os.environ.get("REDDIT_USERNAME")
 REDDIT_PASSWORD = os.environ.get("REDDIT_PASSWORD")
 REDDIT_USER_AGENT = os.environ.get("REDDIT_USER_AGENT", f"COMP90024_manual_harvester by /u/{REDDIT_USERNAME or 'unknown_user'}")
+'''
 
 # --- Fission Pipeline 配置 ---
 FISSION_ENQUEUE_URL = os.environ.get("FISSION_ENQUEUE_URL") # 例如: http://localhost:9090/enqueue/reddit_data
